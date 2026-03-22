42e9e96968792c837fa15c692049bcd3d066c4d4
BreserkGuts <aviral.2428cseai259@kiet.edu>
2026-03-22 09:53:34 +0530

    Done

---
+++ Backend/src/routes/auth.routes.js
@@ -0,0 +1,38 @@
+const express = require("express")
+const authController = require("../controllers/auth.controller")
+const authMiddleware = require("../middleware/auth.middleware")
+
+const authRouter = express.Router();
+
+/**
+ * @route POST /api/auth/register
+ * @description Register a new user
+ * @access Public
+ */
+
+authRouter.post("/register",authController.registerUserController)
+
+/**
+ * @route POST /api/auth/login
+ * @description login user with email and password
+ * @access Public
+ */
+
+authRouter.post("/login",authController.loginUserController);
+
+/**
+ * @route GET /api/auth/logut
+ * @description clear token from user cookie and add token in the blacklist
+ * @access Public
+ */
+
+authRouter.get("/logout",authController.logoutUserController)
+
+/**
+ * @route GET /api/auth/get-me
+ * @description get the current logged in user details
+ * @access Private
+ */
+
+authRouter.get("/get-me",authMiddleware.authUser,authController.getMeController)
+module.exports = authRouter
